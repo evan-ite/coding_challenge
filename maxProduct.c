512808d88e7a01dc645734709cf6a1904b8b3208
mariia paraskun <mariiaparaskun@gmail.com>
2024-05-08 11:18:27 +0200

    macros

--- maxProduct.c
+++ maxProduct.c
@@ -1,21 +1,15 @@
 // C program to find Maximum Product Subarray
 #include <stdio.h>
+#define MAX(a, b) ((a > b) ? a : b)
+#define MIN(a, b) ((a < b) ? a : b)
 
-// Find maximum between two numbers.
-int max(int num1, int num2)
-{
-	return (num1 > num2) ? num1 : num2;
-}
-
-// Find minimum between two numbers.
-int min(int num1, int num2)
-{
-	return (num1 > num2) ? num2 : num1;
-}
 
 /* Returns the product of max product subarray. */
-int maxSubarrayProduct(int arr[], int n)
+int maxProductSubarray(int arr[], int n)
 {
+	if (n < 1) {
+		return 0;
+	}
 	// max positive product
 	// ending at the current position
 	int max_ending_here = arr[0];
@@ -33,25 +27,13 @@ int maxSubarrayProduct(int arr[], int n)
 	and the min product ending previously. */
 	for (int i = 1; i < n; i++) {
 		int temp
-			= max(max(arr[i], arr[i] * max_ending_here),
+			= MAX(MAX(arr[i], arr[i] * max_ending_here),
 				arr[i] * min_ending_here);
 		min_ending_here
-			= min(min(arr[i], arr[i] * max_ending_here),
+			= MIN(MIN(arr[i], arr[i] * max_ending_here),
 				arr[i] * min_ending_here);
 		max_ending_here = temp;
-		max_so_far = max(max_so_far, max_ending_here);
+		max_so_far = MAX(max_so_far, max_ending_here);
 	}
 	return max_so_far;
 }
-
-// Driver code
-int main()
-{
-	int arr[] = { 1, -2, -3, 0, 7, -8, -2 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	printf("Maximum Sub array product is %d",
-		maxSubarrayProduct(arr, n));
-	return 0;
-}
-
-// This code is contributed by Aditya Kumar (adityakumar129)
